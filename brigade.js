903425fba09ec17af84e2185ef3ef9c694c49e85
nxvishal <vishal.tewatia@successive.tech>
2020-02-07 02:18:19 +0530

    update

--- brigade.js
+++ brigade.js
@@ -2,26 +2,26 @@ const { events, Job } = require("brigadier");
 
 events.on("simpleevent", (e, p) => {
 
-  // var deployJob = new Job("build", "alpine");
-  // deployJob.tasks = [
-  //   "cd src",
-  //   "ls -lart"
-  // ];
-  // deployJob.env = {
-  //   "EVENT_TYPE": e.type
-  // };
-  // deployJob.run();
-  try {
+  var deployJob = new Job("build", "alpine");
+  deployJob.storage.enabled = true;
+  deployJob.tasks = [
+    "cd src",
+    "cp helloworld.js mnt/brigade/share/"
+  ];
+  deployJob.env = {
+    "EVENT_TYPE": e.type
+  };
+  deployJob.run();
 
-    var package = new Job("package", "localhost:5000/docker");
-    package.docker.enabled = true
 
-    package.tasks = [
-      "docker images",
-    ];
+  var package = new Job("package", "localhost:5000/docker");
+  package.docker.enabled = true;
+  package.storage.enabled = true;
 
-    package.run()
-  } catch (e) {
-    console.log(e)
-  }
+  package.tasks = [
+    "cat /mnt/brigade/share/helloworld.js",
+  ];
+
+  package.run()
+
 });
