bd2b7c5c4701d8c7fec03f3a50f42880b5961902
nxvishal <vishal.tewatia@successive.tech>
2020-02-07 00:59:10 +0530

    update

--- brigade.js
+++ brigade.js
@@ -2,26 +2,30 @@ const { events, Job } = require("brigadier");
 
 events.on("simpleevent", (e, p) => {
 
-  var deployJob = new Job("build", "alpine");
-  deployJob.tasks = [
-    "cd src",
-    "ls -lart"
-  ];
-  deployJob.env = {
-    "EVENT_TYPE": e.type
-  };
-  deployJob.run();
+  // var deployJob = new Job("build", "alpine");
+  // deployJob.tasks = [
+  //   "cd src",
+  //   "ls -lart"
+  // ];
+  // deployJob.env = {
+  //   "EVENT_TYPE": e.type
+  // };
+  // deployJob.run();
+  try {
 
-  var packageJob = new Job("package", "docker:dind");
-  packageJob.privileged = true;
-  // packageJob.env = {
-  //   DOCKER_DRIVER: "overlay"
-  // }
-  packageJob.tasks = [
-    "dockerd-entrypoint.sh &",
-    "sleep 20",
-    "docker --help",
-  ];
+    var packageJob = new Job("package", "docker:dind");
+    packageJob.privileged = true;
+    // packageJob.env = {
+    //   DOCKER_DRIVER: "overlay"
+    // }
+    packageJob.tasks = [
+      "dockerd-entrypoint.sh &",
+      "sleep 20",
+      "docker --help",
+    ];
 
-  packageJob.run()
+    packageJob.run()
+  } catch (e) {
+    console.log(e)
+  }
 });
