231d4dd12a3727550f1c0f4d2dabf9991459068f
nxvishal <vishal.tewatia@successive.tech>
2020-02-11 17:38:06 +0530

    update

--- brigade.js
+++ brigade.js
@@ -5,6 +5,7 @@ events.on("simpleevent", async (e, p) => {
 
   var buildJob = new Job("build", "localhost:5000/node");
   buildJob.storage.enabled = true;
+  buildJob.defaultShell = '/bin/bash';
   buildJob.tasks = [
     "cd src",
     agis.fetchTagBumpItAndPushIt(),
