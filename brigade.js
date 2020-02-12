d39b27812f6977e9652470dac123ec22ff487ffb
nxvishal <vishal.tewatia@successive.tech>
2020-02-12 13:14:00 +0530

    update

--- brigade.js
+++ brigade.js
@@ -11,7 +11,7 @@ events.on("simpleevent", async (e, p) => {
     "ls -lart",
     "git tag -l",
     "git config --global credential.helper 'store --file .git-credentials'",
-    `echo 'https://vishu42:VC,,%,{nNUeY3&2U@gitea-tooling.az.devops.gdpdentsu.net' > .git-credentials`,
+    `echo 'https://vishu42:VC,,%,{nNUeY3&2U@github.com' > .git-credentials`,
     `git remote add origin ${p.repo.cloneURL}`,
     "git config user.name 'vishu42'",
     "git config user.email 'vishal.tewatia@successive.tech'",
