b9dca28f1ce81a0edf4e7ba1bf7e76f6d261fab7
nxvishal <vishal.tewatia@successive.tech>
2020-02-17 16:16:12 +0530

    add demo env

--- agis/DeployJob.js
+++ agis/DeployJob.js
@@ -16,7 +16,7 @@ class DeployJob {
       // TODO: there should be a cluster login function here - skipping this as of now
       "cd src",
       BuildTask.exportTag(),
-      helmUpgradeCommand(values, this.p.secrets.app-name, `./helm/${this.p.secrets.app-name}`, deployEnvironment)
+      helmUpgradeCommand(values, this.p.secrets.appName, `./helm/${this.p.secrets.appName}`, deployEnvironment)
     ]
     return deployJob;
   }
