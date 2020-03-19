33068cc9e9bbc402e42c10b9f522cc30e2446924
nxvishal <vishal.tewatia@successive.tech>
2020-03-19 17:39:49 +0530

    removed node_modules and agis and add them to gitignore

--- agis/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  BuildTask: require('./BuildTask'),
-  PackageJob: require('./PackageJob'),
-  helmUpgradeCommand: require('./HelmUpgradeCommand'),
-  DeployJob: require('./DeployJob'),
-  Events: require('./Events'),
-}
--- test.js
+++
@@ -1,3 +0,0 @@
-const deployEnvironment = 'kube-ecosystem01-dev'
-const deployEnvSuffix = deployEnvironment.split('-').pop()
-console.log(deployEnvSuffix)
